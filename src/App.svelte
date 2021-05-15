723ecdf2aa3eeb05b361f51aee1be607233f2886
tranthaison1231 <thaison.coderstokyo@gmail.com>
2021-05-15 20:25:45 +0700

    Init project

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte';
+  import TailwindCss from './TailwindCSS.svelte';
+</script>
+
+<TailwindCss />
+<main>
+  <div class="rounded bg-red-900 p-6">
+    <h1 class="m-2">Hello</h1>
+  </div>
+  <Counter />
+</main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let count: number = 0;
+  const increment = () => (count += 1);
+</script>
+
+<button on:click={increment} class="bg-red-900 mt-3 p-4 rounded-xl">
+  Clicks: {count}
+</button>
